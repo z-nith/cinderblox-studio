11faf2455ff873c31867e3db66aa86f8b89ed482
Jesse Meza <jmeza081@gmail.com>
2020-06-14 18:41:41 -0500

    add tests

--- test/layouts/Home.spec.tsx
+++ test/layouts/Home.spec.tsx
@@ -4,15 +4,23 @@ import { ThemeProvider } from '@material-ui/core';
 import theme from 'theme';
 import { Home } from 'layouts';
 import { HomePropTypes } from 'layouts/Home/Home';
+import { mockNextUseRouter } from '../mocks';
 
 describe(`Home`, () => {
+    // Mocks Next.js route
+    mockNextUseRouter({
+        route: '/',
+        pathname: '/',
+        query: '',
+        asPath: `/`,
+    });
+
     const HomeWithTheme = (props: HomePropTypes) => (
         <ThemeProvider theme={theme}>
             <Home {...props} />
         </ThemeProvider>
     );
 
-    // TODO tests here
     it(`should render without error`, () => {
         const wrapper = mount(<HomeWithTheme />);
         expect(wrapper).toMatchSnapshot();
