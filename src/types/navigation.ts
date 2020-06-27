bc23de02bd35d9d653c5297bebf01ccf47e9a467
Jesse Meza <jmeza081@gmail.com>
2020-06-27 13:51:43 -0500

    add navigation types

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,6 +1,12 @@
-export type navigationLink = {
+import React from 'react';
+
+export type NavigationLink = {
     external?: boolean;
     href: string;
     as?: string;
     displayText: string;
 };
+
+export interface NavigationLinkWithIcon extends NavigationLink {
+    icon?: React.ReactNode;
+}
