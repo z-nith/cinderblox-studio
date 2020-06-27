b393331162c37118536e43104c4e80acaf53b235
Jesse Meza <jmeza081@gmail.com>
2020-06-27 17:52:14 -0500

    implement tests

--- test/atoms/Button.spec.tsx
+++ test/atoms/Button.spec.tsx
@@ -3,20 +3,37 @@ import { mount } from 'enzyme';
 import { ThemeProvider } from '@material-ui/core';
 import theme from 'theme';
 import { Button } from 'atoms';
-//import { ButtonPropTypes } from 'atoms/Button/Button';
+import { ButtonPropTypes } from 'atoms/Button/Button';
 
 describe(`Button`, () => {
-    // const ButtonWithTheme = (props: ButtonPropTypes) => (
-    //     <ThemeProvider theme={theme}>
-    //         <Button {...props} />
-    //     </ThemeProvider>
-    // );
+    const ButtonWithTheme = (props: ButtonPropTypes) => (
+        <ThemeProvider theme={theme}>
+            <Button {...props} />
+        </ThemeProvider>
+    );
 
-    // TODO tests here
-    it(`should render without error`, () => {
-        // const wrapper = mount(
-        //     <ButtonWithTheme/>,
-        // );
-        expect(4).toBeLessThanOrEqual(5);
+    it(`should render default button without error`, () => {
+        const wrapper = mount(
+            <ButtonWithTheme onClick={() => {}}>Clickable!</ButtonWithTheme>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it(`should render glow button without error`, () => {
+        const wrapper = mount(
+            <ButtonWithTheme addGlow={true} onClick={() => {}} paddingRight={false}>
+                Clickable!
+            </ButtonWithTheme>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it(`should render padding right button without error`, () => {
+        const wrapper = mount(
+            <ButtonWithTheme addGlow={false} onClick={() => {}} paddingRight>
+                Clickable!
+            </ButtonWithTheme>,
+        );
+        expect(wrapper).toMatchSnapshot();
     });
 });
