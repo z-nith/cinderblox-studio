749221f3279c49eed826f2565c2faedcadff66e5
Jesse Meza <jmeza081@gmail.com>
2020-06-27 13:51:36 -0500

    add container pages

---
+++ src/pages/licenses.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Licenses = () => {
+    return (
+        <div
+            style={{
+                margin: '72px auto',
+            }}
+        >
+            <Typography variant={'h1'}>3rd Party Licenses</Typography>
+        </div>
+    );
+};
+
+export default Licenses;
---
+++ src/pages/terms-of-use.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const TermsOfUse = () => {
+    return (
+        <div
+            style={{
+                margin: '72px auto',
+            }}
+        >
+            <Typography variant={'h1'}>Cinderblox Terms of Use</Typography>
+        </div>
+    );
+};
+
+export default TermsOfUse;
