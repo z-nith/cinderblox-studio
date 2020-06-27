91c7b5c77fbb84e00e7eb3fd8cc479487a1ca62e
Jesse Meza <jmeza081@gmail.com>
2020-06-27 13:49:43 -0500

    add all nav links

---
+++ src/config/Nav.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+    Book,
+    File,
+    GitHub as GitHubFeather,
+    Home as HomeFeather,
+    Package,
+    Server,
+    Info,
+} from 'react-feather';
+import { NavigationLinkWithIcon } from 'types/navigation';
+
+// TODO: Add proper href here
+export const ApiDocs: NavigationLinkWithIcon = {
+    href: 'https://google.com',
+    displayText: 'API Docs',
+    external: true,
+    icon: <Book color={'white'} />,
+};
+
+export const App: NavigationLinkWithIcon = {
+    href: '/color-studio',
+    displayText: 'App',
+    icon: <Server color={'white'} />,
+};
+
+export const Releases: NavigationLinkWithIcon = {
+    href: 'https://headwayapp.co/cinderblox-studio-release-notes',
+    displayText: 'Releases',
+    external: true,
+    icon: <Package color={'white'} />,
+};
+
+export const Home: NavigationLinkWithIcon = {
+    href: '/',
+    displayText: 'Home',
+    icon: <HomeFeather color={'white'} />,
+};
+
+export const Licenses: NavigationLinkWithIcon = {
+    href: '/licenses',
+    displayText: 'Licenses',
+    icon: <File color={'white'} />,
+};
+
+export const TermsOfUse: NavigationLinkWithIcon = {
+    href: '/terms-of-use',
+    displayText: 'Terms of use',
+    icon: <Info color={'white'} />,
+};
+
+export const GitHub: NavigationLinkWithIcon = {
+    href: 'https://github.com/z-nith/cinderblox-studio',
+    displayText: 'GitHub',
+    icon: <GitHubFeather color={'white'} />,
+    external: true,
+};
+
+export const Author: NavigationLinkWithIcon = {
+    href: 'https://github.com/Jmeza081',
+    displayText: 'More projects from Jesse',
+    external: true,
+    icon: <GitHubFeather color={'white'} />,
+};
