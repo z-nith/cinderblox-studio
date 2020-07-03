e924fc0eeb4086fb475dfafd655ea6f3fd12c689
Jesse Meza <jmeza081@gmail.com>
2020-07-03 16:30:20 -0500

    add Lading header

--- src/organisms/index.ts
+++ src/organisms/index.ts
@@ -1,5 +1,6 @@
 import Drawer from './Drawer';
 import Footer from './Footer';
+import LandingHeader from './LandingHeader';
 import Nav from './Nav';
 
-export { Drawer, Nav, Footer };
+export { Drawer, Nav, Footer, LandingHeader };
