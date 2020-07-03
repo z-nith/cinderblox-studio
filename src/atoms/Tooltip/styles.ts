428643dadb8de5c75a8d57b9d76cbfae4e24929c
Jesse Meza <jmeza081@gmail.com>
2020-07-03 16:28:32 -0500

    add custom border radius

--- src/atoms/Tooltip/styles.ts
+++ src/atoms/Tooltip/styles.ts
@@ -4,6 +4,7 @@ const styles: any = (theme: Theme | any) => ({
     tooltip: {
         backgroundColor: theme.palette.custom.charcoalDark,
         color: theme.palette.common.white,
+        borderRadius: '25px',
     },
     arrow: {
         color: theme.palette.custom.charcoalDark,
