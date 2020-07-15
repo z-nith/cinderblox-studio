13496f5dfe9289ff9aacba55c60807e52c7fa770
Jesse Meza <jmeza081@gmail.com>
2020-07-15 14:50:15 -0500

    add Landing Section component

--- src/organisms/index.ts
+++ src/organisms/index.ts
@@ -1,6 +1,7 @@
 import Drawer from './Drawer';
 import Footer from './Footer';
 import LandingHeader from './LandingHeader';
+import LandingSection from './LandingSection';
 import Nav from './Nav';
 
-export { Drawer, Nav, Footer, LandingHeader };
+export { Drawer, Nav, Footer, LandingHeader, LandingSection };
