254c4ae4122ae6149588a18a8458fe6520f91fe2
Jesse Meza <jmeza081@gmail.com>
2020-07-15 14:49:56 -0500

    Finish Starting Layout

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -1,128 +1,244 @@
 import React from 'react';
-import { List, ListItem, ListItemAvatar, ListItemText, NoSsr } from '@material-ui/core';
-import { Button, ListItemCounter, ScrollToTopButton, Typography } from 'atoms';
-import ContentMarginContianer from 'atoms/ContentMarginContainer';
-import { Footer, LandingHeader, Nav } from 'organisms';
-import { ExternalLink, GitHub } from 'react-feather';
+import {
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    useMediaQuery,
+    useTheme,
+} from '@material-ui/core';
+import { BaseLink, Button, ListItemCounter, ScrollToTopButton, Typography } from 'atoms';
+import { ApiDocs, App, GitHub as ProjectGitHubLink, LearnMore } from 'config/Nav';
+import { Footer, LandingHeader, LandingSection, Nav } from 'organisms';
+import { Book, ExternalLink, GitHub } from 'react-feather';
+import ApiIllustration from '../../../public/img/web_development.svg';
+import Analytics from '../../../public/img/innovation_isometric.svg';
+import Presentation from '../../../public/img/project_presentation__isometric.svg';
 import useStyles from './styles';
+import clsx from 'clsx';
 
 const Home = (props: HomePropTypes) => {
     const classes = useStyles();
 
-    const renderFooter = () => {
-        const footerLinks = [];
-        return <Footer />;
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const renderIntroSection = () => {
+        return (
+            <LandingSection
+                flexRowDirection={'row-reverse'}
+                rightChild={<Presentation className={classes.presentation} />}
+                rightGridItemProps={{
+                    xs: 12,
+                    md: 7,
+                }}
+                leftGridItemProps={{
+                    xs: 12,
+                    md: 5,
+                }}
+                leftChild={
+                    <div className={classes.textContentContainer}>
+                        <Typography
+                            variant={'h3'}
+                            color={'textPrimary'}
+                            align={isMobile ? 'center' : 'left'}
+                            gutterBottom
+                        >
+                            Inspiration through technology
+                        </Typography>
+                        <Typography
+                            variant={'body1'}
+                            color={'textSecondary'}
+                            gutterBottom
+                            className={classes.textDescriptionContainer}
+                            align={isMobile ? 'center' : 'left'}
+                        >
+                            Discover the power of Color Quantization via the Cinderblox
+                            Color Studio. By leveraging open-source image analysis tools
+                            you can turn a given image into a beautiful color palette for
+                            your next creative project!
+                        </Typography>
+                        <div className={classes.landingSectionCTAButtonContainer}>
+                            <Button
+                                variant={'contained'}
+                                color={'primary'}
+                                component={BaseLink}
+                                href={App.href}
+                                as={App.as}
+                            >
+                                Find your palette
+                            </Button>
+                        </div>
+                    </div>
+                }
+            />
+        );
     };
 
     const renderHowItWorks = () => {
         return (
-            <div
-                style={{
-                    maxWidth: '525px',
+            <LandingSection
+                flexRowDirection={'row'}
+                backgroundColor={'paper'}
+                leftChild={
+                    <div className={classes.textContentContainer}>
+                        <List
+                            component={'ol'}
+                            subheader={
+                                <Typography variant={'h3'}>How it works</Typography>
+                            }
+                        >
+                            <ListItem alignItems={'flex-start'} disableGutters>
+                                <ListItemAvatar>
+                                    <ListItemCounter count={1} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primaryTypographyProps={{
+                                        color: 'textSecondary',
+                                    }}
+                                    primary={
+                                        'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
+                                    }
+                                />
+                            </ListItem>
+
+                            <ListItem alignItems={'flex-start'} disableGutters>
+                                <ListItemAvatar>
+                                    <ListItemCounter count={2} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primaryTypographyProps={{
+                                        color: 'textSecondary',
+                                    }}
+                                    primary={
+                                        'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
+                                    }
+                                />
+                            </ListItem>
+
+                            <ListItem alignItems={'flex-start'} disableGutters>
+                                <ListItemAvatar>
+                                    <ListItemCounter count={3} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primaryTypographyProps={{
+                                        color: 'textSecondary',
+                                    }}
+                                    primary={
+                                        'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
+                                    }
+                                />
+                            </ListItem>
+                        </List>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'flex-end',
+                                paddingTop: '32px',
+                            }}
+                        >
+                            <Button
+                                variant={'contained'}
+                                color={'primary'}
+                                component={'a'}
+                                href={ProjectGitHubLink.href}
+                                startIcon={<GitHub color="white" size={18} />}
+                                paddingRight
+                                addGlow
+                            >
+                                {ProjectGitHubLink.displayText}
+                            </Button>
+                            <Button
+                                variant={'contained'}
+                                color={'secondary'}
+                                component={'a'}
+                                href={LearnMore.href}
+                                startIcon={<ExternalLink color="black" size={18} />}
+                            >
+                                {LearnMore.displayText}
+                            </Button>
+                        </div>
+                    </div>
+                }
+                leftGridItemProps={{
+                    xs: 12,
+                    md: 6,
                 }}
-            >
-                <List
-                    component={'ol'}
-                    subheader={<Typography variant={'h4'}>How it works</Typography>}
-                >
-                    <ListItem alignItems={'flex-start'} disableGutters>
-                        <ListItemAvatar>
-                            <ListItemCounter count={1} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primaryTypographyProps={{
-                                color: 'textSecondary',
-                            }}
-                            primary={
-                                'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
-                            }
-                        />
-                    </ListItem>
+                rightChild={<Analytics className={classes.presentation} />}
+                rightGridItemProps={{
+                    xs: 12,
+                    md: 6,
+                }}
+            />
+        );
+    };
 
-                    <ListItem alignItems={'flex-start'} disableGutters>
-                        <ListItemAvatar>
-                            <ListItemCounter count={2} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primaryTypographyProps={{
-                                color: 'textSecondary',
-                            }}
-                            primary={
-                                'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
-                            }
-                        />
-                    </ListItem>
-
-                    <ListItem alignItems={'flex-start'} disableGutters>
-                        <ListItemAvatar>
-                            <ListItemCounter count={3} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primaryTypographyProps={{
-                                color: 'textSecondary',
-                            }}
-                            primary={
-                                'Culpa aliquid quos vel totam quia sequi voluptates fuga sunt. Amet ut possimus dolor maxime dolor sunt ipsum. Id error possimus veniam sed doloribus.'
-                            }
-                        />
-                    </ListItem>
-                </List>
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        paddingTop: '32px',
-                    }}
-                >
-                    <Button
-                        variant={'contained'}
-                        color={'primary'}
-                        onClick={() => {}}
-                        startIcon={<GitHub color="white" size={18} />}
-                        paddingRight
-                        addGlow
-                    >
-                        Github
-                    </Button>
-                    <Button
-                        variant={'contained'}
-                        color={'secondary'}
-                        onClick={() => {}}
-                        startIcon={<ExternalLink color="black" size={18} />}
-                    >
-                        Learn more
-                    </Button>
-                </div>
-            </div>
+    const renderApiSection = () => {
+        return (
+            <LandingSection
+                flexRowDirection={'row-reverse'}
+                rightChild={
+                    <ApiIllustration
+                        className={clsx(classes.presentation, classes.apiIllustration)}
+                    />
+                }
+                rightGridItemProps={{
+                    xs: 12,
+                    md: 7,
+                }}
+                leftGridItemProps={{
+                    xs: 12,
+                    md: 5,
+                }}
+                leftChild={
+                    <div className={classes.textContentContainer}>
+                        <Typography
+                            variant={'h3'}
+                            color={'textPrimary'}
+                            gutterBottom
+                            align={isMobile ? 'center' : 'left'}
+                        >
+                            Build something with the CinderBlox API
+                        </Typography>
+                        <Typography
+                            variant={'body1'}
+                            color={'textSecondary'}
+                            gutterBottom
+                            align={isMobile ? 'center' : 'left'}
+                            className={classes.textDescriptionContainer}
+                        >
+                            CinderBlox offers a public API endpoint as a service too!
+                        </Typography>
+                        <div className={classes.landingSectionCTAButtonContainer}>
+                            <Button
+                                variant={'contained'}
+                                color={'primary'}
+                                startIcon={<Book color="white" size={18} />}
+                                component={'a'}
+                                href={ApiDocs.href}
+                            >
+                                See the docs
+                            </Button>
+                        </div>
+                    </div>
+                }
+            />
         );
     };
 
     return (
         <div>
             <Nav />
-            <NoSsr>
-                <LandingHeader />
-            </NoSsr>
+            <ScrollToTopButton {...props} />
 
-            <ContentMarginContianer className={classes.addMarginBottom}>
-                <div
-                    style={{
-                        minHeight: '1400px',
-                        width: '100%',
-                    }}
-                ></div>
-                <Typography variant={'h4'} color={'textPrimary'}>
-                    This is some primary text
-                </Typography>
-                <Typography variant={'body1'} color={'textSecondary'}>
-                    This is some secondary text
-                </Typography>
-                {renderHowItWorks()}
-                <ScrollToTopButton {...props} />
-            </ContentMarginContianer>
-            {renderFooter()}
+            <LandingHeader />
+
+            {renderIntroSection()}
+            {renderHowItWorks()}
+            {renderApiSection()}
+            <Footer />
         </div>
     );
 };
--- src/layouts/Home/styles.ts
+++ src/layouts/Home/styles.ts
@@ -4,6 +4,33 @@ const styles: any = (theme: Theme | any) => ({
     addMarginBottom: {
         paddingBottom: theme.spacing(3),
     },
+    presentation: {
+        height: 'auto',
+        width: '100%',
+    },
+    apiIllustration: {
+        paddingRight: theme.spacing(12),
+        [theme.breakpoints.down('sm')]: {
+            paddingRight: theme.spacing(3),
+        },
+    },
+    textContentContainer: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: theme.spacing(0, 1),
+        [theme.breakpoints.down('sm')]: {
+            alignItems: 'center',
+            paddingBottom: theme.spacing(8),
+        },
+    },
+    textDescriptionContainer: {
+        paddingTop: theme.spacing(3),
+    },
+    landingSectionCTAButtonContainer: {
+        paddingTop: theme.spacing(4),
+    },
 });
 
 export default makeStyles(styles);
