400f3c53e0870b3c425414d29e7496d4e5981725
Jesse Meza <jmeza081@gmail.com>
2020-07-15 14:49:28 -0500

    make FAB always on top

--- src/atoms/ScrollToTopButton/styles.ts
+++ src/atoms/ScrollToTopButton/styles.ts
@@ -2,6 +2,7 @@ import { Theme, makeStyles } from '@material-ui/core';
 
 const styles: any = (theme: Theme | any) => ({
     root: {
+        zIndex: 9999,
         position: 'fixed',
         bottom: theme.spacing(3),
         right: theme.spacing(3),
